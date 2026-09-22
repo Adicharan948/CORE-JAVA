3f06b7ae385449a50ddf614af9435ecf58983842
Adi charan <adicharan988@gmail.com>
2026-09-22 09:28:15 +0530

    push to git

---
+++ myfirstjavaproject/src/com/javaintroduction/DEMO.java
@@ -0,0 +1,16 @@
+package com.javaintroduction;
+
+public class DEMO {
+	
+	static {
+		System.out.println("ghus");
+	}
+
+	public static void main(String[] args) {
+		
+		System.out.println(78);
+		
+
+	}
+
+}
