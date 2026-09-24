481348ca54e469eda1fa9cc149cc152840c213c4
Adi charan <adicharan988@gmail.com>
2026-09-24 11:16:28 +0530

    BLOACKS AND METHODS

---
+++ myfirstjavaproject/src/com/languagefundamentals/Student.java
@@ -0,0 +1,52 @@
+package com.languagefundamentals;
+
+class Student {
+
+    int rollNo;
+    String name;
+    int marks;
+
+  
+    static {
+        System.out.println("College Name:saveetha");
+    }
+
+  
+    {
+        System.out.println("Student object created");
+    }
+
+   
+    Student(int rollNo, String name, int marks) {
+        this.rollNo = rollNo;
+        this.name = name;
+        this.marks = marks;
+    }
+
+   //instance method
+    void displayStudentDetails() {
+        System.out.println("Roll No: " + rollNo);
+        System.out.println("Name: " + name);
+        System.out.println("Marks: " + marks);
+    }
+
+   //Static method
+    static void displayCollegeDetails() {
+        
+    }
+
+    public static void main(String[] args) {
+
+       
+        Student s1 = new Student(101, "Rahul", 85);
+
+       
+        Student s2 = new Student(102, "Priya", 90);
+
+     
+        s1.displayStudentDetails();
+        s2.displayStudentDetails();
+
+       
+    }
+}
